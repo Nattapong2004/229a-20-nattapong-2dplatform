1835e428ad7943e5176dfc65e8c7fc6c5b03919c
Nattapong2004 <nattapong.buph@bumail.net>
2024-09-24 11:16:33 +0700

    ทำAbstraction

---
+++ Assets/Scripts/Lab/Ant.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ant : Enemy
+{
+    private Vector2 velocity;
+
+    public override void Behavior()
+    {
+        Debug.Log("Ant Behavior");
+    }
+    
+}
